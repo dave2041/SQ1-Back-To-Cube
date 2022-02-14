ad2e0aecca09d13507e9f6c5abdcda8c1f579a09
dave2041 <davidmstammers@gmail.com>
2022-02-14 09:19:47 +0000

    Initial commit

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "0a.gif");
+            this.imageList1.Images.SetKeyName(1, "10a.gif");
+            this.imageList1.Images.SetKeyName(2, "10b.gif");
+            this.imageList1.Images.SetKeyName(3, "11a.gif");
+            this.imageList1.Images.SetKeyName(4, "11b.gif");
+            this.imageList1.Images.SetKeyName(5, "11c.gif");
+            this.imageList1.Images.SetKeyName(6, "12a.gif");
+            this.imageList1.Images.SetKeyName(7, "12b.gif");
+            this.imageList1.Images.SetKeyName(8, "13a.gif");
+            this.imageList1.Images.SetKeyName(9, "13b.gif");
+            this.imageList1.Images.SetKeyName(10, "14a.gif");
+            this.imageList1.Images.SetKeyName(11, "14b.gif");
+            this.imageList1.Images.SetKeyName(12, "15a.gif");
+            this.imageList1.Images.SetKeyName(13, "15b.gif");
+            this.imageList1.Images.SetKeyName(14, "15c.gif");
+            this.imageList1.Images.SetKeyName(15, "15d.gif");
+            this.imageList1.Images.SetKeyName(16, "16a.gif");
+            this.imageList1.Images.SetKeyName(17, "16b.gif");
+            this.imageList1.Images.SetKeyName(18, "16c.gif");
+            this.imageList1.Images.SetKeyName(19, "16d.gif");
+            this.imageList1.Images.SetKeyName(20, "17a.gif");
+            this.imageList1.Images.SetKeyName(21, "17b.gif");
+            this.imageList1.Images.SetKeyName(22, "17c.gif");
+            this.imageList1.Images.SetKeyName(23, "17d.gif");
+            this.imageList1.Images.SetKeyName(24, "17e.gif");
+            this.imageList1.Images.SetKeyName(25, "18a.gif");
+            this.imageList1.Images.SetKeyName(26, "18b.gif");
+            this.imageList1.Images.SetKeyName(27, "18c.gif");
+            this.imageList1.Images.SetKeyName(28, "18d.gif");
+            this.imageList1.Images.SetKeyName(29, "18e.gif");
+            this.imageList1.Images.SetKeyName(30, "19a.gif");
+            this.imageList1.Images.SetKeyName(31, "19b.gif");
+            this.imageList1.Images.SetKeyName(32, "19c.gif");
+            this.imageList1.Images.SetKeyName(33, "19d.gif");
+            this.imageList1.Images.SetKeyName(34, "1a.gif");
+            this.imageList1.Images.SetKeyName(35, "20a.gif");
+            this.imageList1.Images.SetKeyName(36, "20b.gif");
+            this.imageList1.Images.SetKeyName(37, "20c.gif");
+            this.imageList1.Images.SetKeyName(38, "20d.gif");
+            this.imageList1.Images.SetKeyName(39, "20e.gif");
+            this.imageList1.Images.SetKeyName(40, "20f.gif");
+            this.imageList1.Images.SetKeyName(41, "20g.gif");
+            this.imageList1.Images.SetKeyName(42, "21a.gif");
+            this.imageList1.Images.SetKeyName(43, "21b.gif");
+            this.imageList1.Images.SetKeyName(44, "21c.gif");
+            this.imageList1.Images.SetKeyName(45, "22a.gif");
+            this.imageList1.Images.SetKeyName(46, "22b.gif");
+            this.imageList1.Images.SetKeyName(47, "22c.gif");
+            this.imageList1.Images.SetKeyName(48, "22d.gif");
+            this.imageList1.Images.SetKeyName(49, "23a.gif");
+            this.imageList1.Images.SetKeyName(50, "23b.gif");
+            this.imageList1.Images.SetKeyName(51, "23c.gif");
+            this.imageList1.Images.SetKeyName(52, "23d.gif");
+            this.imageList1.Images.SetKeyName(53, "23e.gif");
+            this.imageList1.Images.SetKeyName(54, "23f.gif");
+            this.imageList1.Images.SetKeyName(55, "24a.gif");
+            this.imageList1.Images.SetKeyName(56, "24b.gif");
+            this.imageList1.Images.SetKeyName(57, "25a.gif");
+            this.imageList1.Images.SetKeyName(58, "25b.gif");
+            this.imageList1.Images.SetKeyName(59, "25c.gif");
+            this.imageList1.Images.SetKeyName(60, "26a.gif");
+            this.imageList1.Images.SetKeyName(61, "26b.gif");
+            this.imageList1.Images.SetKeyName(62, "26c.gif");
+            this.imageList1.Images.SetKeyName(63, "26d.gif");
+            this.imageList1.Images.SetKeyName(64, "27a.gif");
+            this.imageList1.Images.SetKeyName(65, "27b.gif");
+            this.imageList1.Images.SetKeyName(66, "28a.gif");
+            this.imageList1.Images.SetKeyName(67, "2a.gif");
+            this.imageList1.Images.SetKeyName(68, "3a.gif");
+            this.imageList1.Images.SetKeyName(69, "4a.gif");
+            this.imageList1.Images.SetKeyName(70, "5a.gif");
+            this.imageList1.Images.SetKeyName(71, "5b.gif");
+            this.imageList1.Images.SetKeyName(72, "5c.gif");
+            this.imageList1.Images.SetKeyName(73, "6a.gif");
+            this.imageList1.Images.SetKeyName(74, "6b.gif");
+            this.imageList1.Images.SetKeyName(75, "6c.gif");
+            this.imageList1.Images.SetKeyName(76, "7a.gif");
+            this.imageList1.Images.SetKeyName(77, "7b.gif");
+            this.imageList1.Images.SetKeyName(78, "8a.gif");
+            this.imageList1.Images.SetKeyName(79, "8b.gif");
+            this.imageList1.Images.SetKeyName(80, "9a.gif");
+            this.imageList1.Images.SetKeyName(81, "9b.gif");
+            // 
+            // listView1
+            // 
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(12, 35);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(665, 498);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.LargeImageList = this.imageList1;
+            this.listView2.Location = new System.Drawing.Point(12, 539);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(455, 389);
+            this.listView2.SmallImageList = this.imageList1;
+            this.listView2.TabIndex = 1;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // listView3
+            // 
+            this.listView3.LargeImageList = this.imageList1;
+            this.listView3.Location = new System.Drawing.Point(681, 35);
+            this.listView3.Margin = new System.Windows.Forms.Padding(1);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(560, 893);
+            this.listView3.SmallImageList = this.imageList1;
+            this.listView3.TabIndex = 2;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1248, 935);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Square -1 Back To Cube";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        List<CubeState> CubeStates = new List<CubeState>();
+        List<string> ImgNames = new List<string>();
+        string selectedTopImageName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // File name  
+            string fileName = @"Data.txt";
+
+            try
+            {
+                // Create a StreamReader  
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string line;
+                    string[] subs;
+                    CubeState CS;
+                    // Read line by line  
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        subs = line.Split(',');
+                        int pair = int.Parse(subs[0]);
+                        string top = subs[1];
+                        string bottom = subs[2];
+                        int next = int.Parse(subs[3]);
+                        int distance = int.Parse(subs[5]);
+                        CS = new CubeState(pair, top, bottom, next, distance);
+                        CubeStates.Add(CS);
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+
+            //var Images= Directory.GetFiles(@".\Images\", "*.gif",
+            //                       SearchOption.AllDirectories)
+            //             .Select(Image.FromFile).ToList();
+
+            ImgNames = Directory.GetFiles(@".\Images\", "*.gif", SearchOption.AllDirectories).ToList();
+            ImgNames.Sort();
+
+            int imgCount = 0;
+
+            foreach (string s in ImgNames)
+            {
+                if (s.Substring(10).Contains('a'))
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.ImageIndex = imgCount;
+                    lvi.Name = ImgNames[imgCount];
+                    listView1.Items.Add(lvi);
+                }
+                imgCount++;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView2.Clear();
+                selectedTopImageName = listView1.SelectedItems[0].Name;
+                string selectedImageName = selectedTopImageName.Replace("[^-?0-9]+", " ");
+                try
+                {
+                    Regex regexObj = new Regex(@"[^\d]");
+                    selectedImageName = regexObj.Replace(selectedImageName, "");
+                }
+                catch (ArgumentException ex)
+                {
+                    // Syntax error in the regular expression
+                }
+                var validStates = CubeStates.Where(CubeState => CubeState.topImageName.Contains(selectedImageName));
+                
+                foreach (CubeState cs in validStates)
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    int index = ImgNames.FindIndex(x => x.Contains(cs.bottomImageName));
+                    lvi.ImageIndex = index;
+                    lvi.Name = ImgNames[index];
+                    listView2.Items.Add(lvi);
+                    //lvi = new ListViewItem();
+                    //index = ImgNames.FindIndex(x => x == cs.bottomImageName);
+                    //lvi.ImageIndex = index;
+                    //listView3.Items.Add(lvi);
+                }
+            }
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0 && listView2.SelectedItems.Count > 0)
+            {
+                listView3.Clear();
+                string selectedBottomImageName = listView2.SelectedItems[0].Name;
+                string selectedImageName = selectedBottomImageName.Replace("[^-?0-9]+", " ");
+                string selectedTopImageNameShort = "";
+                try
+                {
+                    Regex regexObj = new Regex(@"[^\d]");
+                    selectedImageName = regexObj.Replace(selectedImageName, "");
+                    selectedTopImageNameShort = regexObj.Replace(selectedTopImageName, "");
+                }
+                catch (ArgumentException ex)
+                {
+                    // Syntax error in the regular expression
+                }
+
+                var validStates = CubeStates.Where(CubeState => CubeState.topImageName.Contains(selectedTopImageNameShort)
+                && CubeState.bottomImageName.Contains(selectedImageName)).ToArray();
+                bool firstTime = true;
+
+                if (validStates.Count() > 0)
+                {
+                    do
+                    {
+                        if (!firstTime)
+                        {
+                            validStates = CubeStates.Where(CubeState => CubeState.PairNo == validStates[0].nextState).ToArray();
+                        }
+
+                        firstTime = false;
+                        listView3.Items.Add("Pair #" + validStates[0].PairNo);
+                        ListViewItem lvi = new ListViewItem();
+                        int index = ImgNames.FindIndex(x => x.Equals(".\\Images\\" + validStates[0].topImageName));
+                        lvi.ImageIndex = index;
+                        listView3.Items.Add(lvi);
+                        lvi = new ListViewItem();
+                        index = ImgNames.FindIndex(x => x.Contains(validStates[0].bottomImageName));
+                        lvi.ImageIndex = index;
+                        listView3.Items.Add(lvi);
+                        listView3.Items.Add(validStates[0].nextState + " Next. Distance: " + validStates[0].distance);
+                        listView3.Items.Add("");
+                    }
+                    while (validStates[0].nextState != 999);
+                }
+
+            }
+        }
+    }
+
+    class CubeState
+    {
+        public int PairNo;
+        public string topImageName;
+        public string bottomImageName;
+        public int nextState;
+        public int distance;
+
+        public CubeState()
+        {
+        }
+
+        public CubeState(int pair, string top, string bottom, int next, int dist)
+        {
+            nextState = next;
+            distance = dist;
+            PairNo = pair;
+            topImageName = top;
+            bottomImageName = bottom;
+        }
+    }
+}
