398e756ca45d2adca836c28216e249002c845fe8
dave2041 <davidmstammers@gmail.com>
2022-02-14 09:39:48 +0000

    Added comments/Tidied code
    Added comments, tidied code a little.

--- WindowsFormsApplication3/Form1.cs
+++ WindowsFormsApplication3/Form1.cs
@@ -21,7 +21,8 @@ namespace WindowsFormsApplication3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // File name  
+            // The data file holds all of the states the cube can be in
+            // Each state is numbered, has a distance to cube, which step is next, top and bottom pics
             string fileName = @"Data.txt";
 
             try
@@ -32,7 +33,7 @@ namespace WindowsFormsApplication3
                     string line;
                     string[] subs;
                     CubeState CS;
-                    // Read line by line  
+                    // Read the data in and convert each line to a cubeState object placing it into a list.
                     while ((line = reader.ReadLine()) != null)
                     {
                         Console.WriteLine(line);
@@ -52,17 +53,15 @@ namespace WindowsFormsApplication3
                 Console.WriteLine(exp.Message);
             }
 
-            //var Images= Directory.GetFiles(@".\Images\", "*.gif",
-            //                       SearchOption.AllDirectories)
-            //             .Select(Image.FromFile).ToList();
-
+            // Grab all the image FileNames
             ImgNames = Directory.GetFiles(@".\Images\", "*.gif", SearchOption.AllDirectories).ToList();
-            ImgNames.Sort();
 
             int imgCount = 0;
 
             foreach (string s in ImgNames)
             {
+                // if the filename has an 'a' in it then we want to use it as the first option of the solve
+                // The letters denote a twist of the puzzle layer.
                 if (s.Substring(10).Contains('a'))
                 {
                     ListViewItem lvi = new ListViewItem();
@@ -76,10 +75,14 @@ namespace WindowsFormsApplication3
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // The user has selected a layer to solve. Search the cubeStates list for a mathcing pair 
+            // and populate the second list with the matching layer options
             if (listView1.SelectedItems.Count > 0)
             {
                 listView2.Clear();
                 selectedTopImageName = listView1.SelectedItems[0].Name;
+
+                // striping file name to just the numbers so orientation doesn't matter.
                 string selectedImageName = selectedTopImageName.Replace("[^-?0-9]+", " ");
                 try
                 {
@@ -90,8 +93,11 @@ namespace WindowsFormsApplication3
                 {
                     // Syntax error in the regular expression
                 }
+
+                // a lookup for a list of valid cubestates
                 var validStates = CubeStates.Where(CubeState => CubeState.topImageName.Contains(selectedImageName));
                 
+                // searching the valid list of cubestates and populating the second list.
                 foreach (CubeState cs in validStates)
                 {
                     ListViewItem lvi = new ListViewItem();
@@ -99,26 +105,23 @@ namespace WindowsFormsApplication3
                     lvi.ImageIndex = index;
                     lvi.Name = ImgNames[index];
                     listView2.Items.Add(lvi);
-                    //lvi = new ListViewItem();
-                    //index = ImgNames.FindIndex(x => x == cs.bottomImageName);
-                    //lvi.ImageIndex = index;
-                    //listView3.Items.Add(lvi);
                 }
             }
         }
 
         private void listView2_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // If we've selected a top and a bottom later to be solved then step through the list of states 
+            // to provide the solution to the puzzle
             if (listView1.SelectedItems.Count > 0 && listView2.SelectedItems.Count > 0)
             {
                 listView3.Clear();
-                string selectedBottomImageName = listView2.SelectedItems[0].Name;
-                string selectedImageName = selectedBottomImageName.Replace("[^-?0-9]+", " ");
+                string selectedBottomImageNameShort = listView2.SelectedItems[0].Name;
                 string selectedTopImageNameShort = "";
                 try
                 {
                     Regex regexObj = new Regex(@"[^\d]");
-                    selectedImageName = regexObj.Replace(selectedImageName, "");
+                    selectedBottomImageNameShort = regexObj.Replace(selectedBottomImageNameShort, "");
                     selectedTopImageNameShort = regexObj.Replace(selectedTopImageName, "");
                 }
                 catch (ArgumentException ex)
@@ -126,20 +129,24 @@ namespace WindowsFormsApplication3
                     // Syntax error in the regular expression
                 }
 
+                // Find the current state using the selected top and bottom images
                 var validStates = CubeStates.Where(CubeState => CubeState.topImageName.Contains(selectedTopImageNameShort)
-                && CubeState.bottomImageName.Contains(selectedImageName)).ToArray();
+                && CubeState.bottomImageName.Contains(selectedBottomImageNameShort)).ToArray();
                 bool firstTime = true;
 
                 if (validStates.Count() > 0)
                 {
                     do
                     {
+                        // ensure we populate with our first state
                         if (!firstTime)
                         {
+                            // Grab the next state of the solve using the "next state" of the current state.
                             validStates = CubeStates.Where(CubeState => CubeState.PairNo == validStates[0].nextState).ToArray();
                         }
 
                         firstTime = false;
+                        // populate the answers list with the state to be solved.
                         listView3.Items.Add("Pair #" + validStates[0].PairNo);
                         ListViewItem lvi = new ListViewItem();
                         int index = ImgNames.FindIndex(x => x.Equals(".\\Images\\" + validStates[0].topImageName));
@@ -152,6 +159,7 @@ namespace WindowsFormsApplication3
                         listView3.Items.Add(validStates[0].nextState + " Next. Distance: " + validStates[0].distance);
                         listView3.Items.Add("");
                     }
+                    // the last states "next state" is 999 to indicate we've reached a square. Stop when we get there.
                     while (validStates[0].nextState != 999);
                 }
 
